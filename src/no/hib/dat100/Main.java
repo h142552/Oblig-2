e09275056ec9484e6f596fe7c199829ebfb15514
Sigbjørn <h142552@stud.hib.no>
2016-09-30 17:00:15 +0200

    Oppgave 4 ferdig

--- src/no/hib/dat100/Main.java
+++ src/no/hib/dat100/Main.java
@@ -2,7 +2,17 @@ package no.hib.dat100;
 
 public class Main {
 	public static void main(String[] args) {
+            // Oppgave 3
             Oppgave3 oppg3 = new Oppgave3();
             oppg3.start();
+
+            // Oppgave 4
+            System.out.print("\n----------------------");
+            Vare v = new Vare(1, "Banan", 3.14f);
+            v.skriv();
+            
+            Vare b = new Vare(2, "Eple", 6.18f);
+            System.out.print("\nEr bananer billigere enn epler? "
+                + v.billigereEnn(b));
 	}
 }
---
+++ src/no/hib/dat100/Vare.java
@@ -0,0 +1,44 @@
+package no.hib.dat100;
+
+public class Vare {
+    private int varenr;
+    private String navn;
+    private float pris;
+
+    public int getVarenr()      { return this.varenr; }
+    public String getNavn()     { return this.navn; }
+    public float getPris()      { return this.pris; }
+    
+    public void setVarenr(int varenr)   { this.varenr = varenr; }
+    public void setNavn(String navn)    { this.navn = navn; }
+    public void setPris(float pris)     { this.pris = pris; }
+
+    public Vare(){
+
+    }
+
+    public Vare(int varenr, String navn, float pris){
+        this.varenr = varenr;
+        this.navn   = navn;
+        this.pris   = pris;
+    }
+    
+    public float moms(){
+        return (float) this.pris / 5.0f;
+    }
+
+    public boolean billigereEnn(Vare v){
+        if(this.pris < v.pris)
+            return true;
+        else
+            return false;
+    }
+
+    public void skriv(){
+        System.out.print(
+            "\nVarenummer: " + this.varenr
+            + "\nNavn: " + this.navn
+            + "\nPris: " + this.pris
+            + "\nMoms: " + this.moms());
+    }
+}
